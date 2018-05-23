3acbc90e031d146e2c5b3dee7d91df2e95514d41
RdL <ronald_de_lange@hotmail.com>
2018-05-23 23:44:01 +0200

    First version

---
+++ README.md
@@ -0,0 +1,60 @@
+## Mosquito Broadcast
+Allow Mycroft to broadcast messages to all other mycroft devices having the broadcast skill installed and listening to the same topic on the same mqtt server.
+
+## Description
+This skill has three purposes:
+1) Broadcast messages to other mycroft devices
+2) Receive broadcast messages from other mycroft devices
+3) Repeat the last broadcast message which was send to the mqtt topic specified
+
+And a warning: Due to the way this skill is implemented, it may stop working in case of a live update, under certain circumstances. A workaround is restarting the skill (or restarting Mycroft).
+The problem only affects this skill. I am still looking for a solution to this.
+
+
+### Configuration
+The skill is configured on your "Mycroft Home" page. Configure the mqtt server, port and topic that the skill will listen for text messages on.
+Currently, password or certificates are not supported. (Maybe I will implement it if you promise to test it :)
+
+A restart of the skill is needed when changing the configuration.
+
+Optionally, it is possible to split the text, using a regular expression.
+
+Example CamelCase: If you send the string "KitchenWindow is open",
+you want to split KitchenWindow. After the split Mycroft will say "Kitchen Window is open". To do that set the parameters on "Mycroft Home" like this:
+* Split text at pattern (optional): [a-z][A-Z]
+* Retain characters in matched string until index: 1
+* Retain characters in matched string from index: 1
+
+What happens: The regex match "nK" in "KitchenWindow is open". We retain the characters until index 1 of "nK", which is n.
+We retain the characters after index 1 of "nK", which is K. And we put a space in the middle.
+
+Example hypen: Convert "Outside-temperature is -5 degrees" to "Outside temperature is -5 degrees"
+* Split text at pattern (optional): [a-z|A-Z]-[a-z|A-Z]
+* Retain characters in matched string until index: 1
+* Retain characters in matched string from index: 2
+
+What happens: The regex match "e-t" in "Outside-temperature is -5 degrees".  We retain the characters until index 1 of "e-t", which is e.
+We retain the characters after index 2 of "e-t", which is t. And we put a space in the middle.
+
+Example underscore: Convert "Kitchen_window is open" to "Kitchen Window is open"
+* Split text at pattern (optional): _
+* Retain characters in matched string until index: 0
+* Retain characters in matched string from index: 1
+
+What happens: The regex match "_" in "Kitchen_window is open".  We retain the characters until index 0 of "_", which is no characters.
+We retain the characters after index 1 of "_", which is no characters. And we put a space in the middle.
+
+
+## Example
+For this example you must have a mqtt server named "mqttserver". And a configuration on mycroft home setting the server to "mqttserver", port to 1883, and topic to "my-out/text"
+Example of broadcast:
+
+* You: "Hey Mycroft, broadcast dinner is ready"
+* Mycroft (on your device): "Your message is broadcasted to all other devices"
+* Mycroft (on other devices): <DINGDONG> "This is a broadcast message, please pay attention. Dinner is ready"
+* You: "Hey Mycroft, last broadcast"
+* Mycroft (on your device): "This is a broadcast message, please pay attention. Dinner is ready"
+
+
+## Credits
+Based on the Mosquito-Speak Skill from Carsten Agerskov (https://github.com/CarstenAgerskov)
---
+++ __init__.py
@@ -0,0 +1,132 @@
+"""
+skill mosquito-speak
+Copyright (C) 2017  Carsten Agerskov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from mycroft import intent_file_handler, intent_handler
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+import paho.mqtt.client as mqtt
+import re
+import time
+from mycroft.messagebus.message import Message
+from mycroft.util.log import LOG
+from os.path import dirname, join
+from mycroft.util import play_mp3
+
+
+__author__ = 'rdl'
+
+try:
+    client
+    LOG.info('Client exist')
+    client.loop_stop()
+    client.disconnect()
+    LOG.info('Stopped old client loop')
+except NameError:
+    client = mqtt.Client()
+    LOG.info('Client created')
+
+
+class MosquitoBroadcast(MycroftSkill):
+    def __init__(self):
+        super(MosquitoBroadcast, self).__init__(name='MosquitoBroadcast')
+        self.host = None
+        self.port = None
+        self.topic = None
+        self.uuid = None
+        self.last_message = None
+        self.splitRegex = None
+        self.retainFirst = None
+        self.retainLast = None
+        self.loop_succeeded = False
+        self.lastbroadcastsend = None
+        self.process = None
+
+    def on_connect(self, client, userdata, flags, rc):
+        client.subscribe(self.topic)
+        if rc == 0:
+            LOG.info('Connected to ' + self.topic)
+        else:
+            LOG.error('Connection to ' + self.topic +
+                      ' failed, error code ' + rc)
+
+    def on_message(self, client, userdata, msg):
+        try:
+            m = msg.payload.decode('utf-8')
+
+            if self.splitRegex:
+                m = re.sub(self.splitRegex, lambda x:
+                    x.group(0)[0:int(self.retainFirst)] +
+                    ' ' + x.group(0)[int(self.retainLast):], m)
+            if (self.lastbroadcastsend != m):
+                self.process = play_mp3(join(dirname(__file__), "broadcast.mp3"))
+                time.sleep(4)
+                self.speak(m)
+                self.last_message = m
+
+        except Exception as e:
+            LOG.error('Error: {0}'.format(e))
+
+    def initialize(self):
+        self.host = self.settings.get('host')
+        self.port = self.settings.get('port')
+        self.topic = self.settings.get('topic')
+        self.splitRegex = self.settings.get('splitRegex')
+        self.retainFirst = self.settings.get('retainFirst')
+        self.retainLast = self.settings.get('retainLast')
+        self.last_message = 'There is no last broadcast'
+        self.loop_succeeded = False
+
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        try:
+            LOG.info("Connecting to host " + self.host + " on port " + self.port)
+            client.connect_async(self.host, int(self.port), 60)
+            client.loop_start()
+            self.loop_succeeded = True
+        except Exception as e:
+            LOG.error('Error: {0}'.format(e))
+
+    @intent_file_handler('RepeatLastBroadcast.intent')
+    def repeat_last_message_intent(self):
+        self.speak(self.last_message)
+
+        if not self.loop_succeeded:
+            self.speak_dialog('not_configured')
+
+    @intent_handler(IntentBuilder("").require("Broadcast").require("Words"))
+    def broadcastmessage(self, message):
+        """
+            Broadcast the utterance to all other devices with the Mosquito Broadcast Skill installed.
+        """
+        # Remove everything up to the broadcast keyword and publish that on the mqtttopic
+        utterance = message.data.get('utterance')
+        repeat = re.sub('^.*?' + message.data['Broadcast'], '', utterance)
+        self.speak("Your message is broadcasted to all other devices.")
+        client.publish(self.topic, "This is a broadcast message, please pay attention. "+repeat.strip())
+        self.lastbroadcastsend = "This is a broadcast message, please pay attention. "+repeat.strip()
+
+    def stop(self):
+        pass
+
+
+def create_skill():
+    return MosquitoBroadcast()
